3bf97ddee68f9ea660392d7a00ab1ca28cf41645
miwanoff <marina.iva.noffa@gmail.com>
2026-02-22 13:27:04 +0200

    Add clearCart

--- script.js
+++ script.js
@@ -26,10 +26,46 @@ function addToCart(event) {
     cartData[itemID] = [itemTitle, itemPrice, 1];
   }
 
-  console.log(cartData);
-  setCartData(cartData)
+//   console.log(cartData);
+  setCartData(cartData);
+}
+
+function removeItem(el) {
+    
+}
+
+function openCart(event) {
+  const cartData = getCartData();
+  console.log(cartData)
+   if (cartData !== null) {
+    let cardTable = "";
+    cardTable =
+      '<table class="shopping_list"><tr><th>Назва</th><th>Ціна</th><th>Кількість</th><th>Додати</th><th>Видалити</th></tr>';
+    for (let item in cartData) {
+      cardTable += "<tr>";
+      for (let i = 0; i < cartData[item].length; i++) {
+        cardTable += "<td>" + cartData[item][i] + "</td>";
+      }
+      cardTable += "<td> + </td>";
+      cardTable += `<td><span class="minus" onclick="removeItem(this)" data-id="${item}">-</span></td>`;
+      cardTable += "</tr>";
+    }
+    cardTable += "<table>";
+    cartCont.innerHTML = cardTable;
+  } else {
+    // если в корзине пусто, то сигнализируем об этом
+    cartCont.innerHTML = "У кошику пусто!";
+  }
+}
+
+function clearCart() {
+  localStorage.removeItem("cart");
+  cartCont.innerHTML = "Кошик очишено.";
 }
 
 for (let i = 0; i < itemBoxes.length; i++) {
   itemBoxes[i].querySelector(".add_item").addEventListener("click", addToCart);
 }
+
+document.getElementById("checkout").addEventListener("click", openCart);
+document.getElementById("clear_cart").addEventListener("click", clearCart);
